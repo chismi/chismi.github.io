0265417741c2aa97fa10a2e259d422c4e33ffa67
Mitch Chisholm <chisholm.mitchell@gmail.com>
2017-06-10 22:04:10 -0400

    Updated Nav and js Events

--- js/index.js
+++ js/index.js
@@ -1,22 +1,82 @@
 $( document ).ready(function(){
 	//fade in background and icons On Loading
-	$('#background').fadeIn(800);
-	$('BLM-icon-idx').fadeIn(1000);
+	$('#background').fadeIn(400);
+	$('.BLM-icon-idx').fadeIn(800);
+	$('.Icon-title').slideDown(400);
 
 });
 
 //Change on selecting Content
-function switchContent() {
+function switchIcon(content) {
 
-//Toggle off Icon & Title
-$('.BLM-icon-idx').toggle();
-$('.title').toggle();
-
-//Toggle On Small Icon & Title
-$('.BLM-icon-sm-bw').slideToggle(400);
-$('.Icon-title').slideToggle(400);
+//Find the Nav Chose
+switch (content) {
+	 /* Work Icon */
+	 case 'Work':
+	 $('.BLM-icon-sm').css({"background":"url(\"img/Icon_idx.png\") no-repeat center center","background-size":"contain"})
+	 break;
+	 /* Development Icon */
+	 case 'In-Development':
+	 $('.BLM-icon-sm').css({"background":"url(\"img/Icon_dev.png\") no-repeat center center","background-size":"contain"})
+	 break;
+	 /* Contributors Icon */
+	 case 'Contributors':
+	 $('.BLM-icon-sm').css({"background":"url(\"img/Icon_wrk.png\") no-repeat center center","background-size":"contain"})
+	 break;
+	 /* Contact Icon */
+	 case 'Contact':
+	 $('.BLM-icon-sm').css({"background":"url(\"img/Icon_bw.png\") no-repeat center center","background-size":"contain"})
+	 break;
 }
 
-$('#content-btn').on('click', function() {
-	switchContent()
+}
+
+//Change Nav Background Color based on type of Nav
+function navColor(navElement) {
+
+var navTitle = navElement.text()
+
+//Find the Nav Chosen
+switch (navTitle) {
+	 /* Return Work Color */
+	 case 'Work':
+	 return "rgba(189,16,224,0.75)";
+	 break;
+	 /* Return Development Color */
+	 case 'In-Development':
+	 return "rgba(0,170,197,0.75)";
+	 break;
+	 /* Return Contributors Color */
+	 case 'Contributors':
+	 return "rgba(248,231,28,0.75)";
+	 break;
+	 /* Contact Icon */
+	 case 'Contact':
+	 return "rgba(126,211,33,0.75)";
+	 break;
+}
+
+}
+
+
+// Establish Nav Button Event Listeners */
+var $navButton = $('li.nav');
+
+// Onclick change Icon and Set Button Background Color
+$navButton.click(function(){
+	switchIcon($(this).text());
 });
+
+// Onmouseover change mouse and Font Weight
+$navButton.mouseover(function(){
+
+	var switchColor = navColor($(this));
+	$(this).css("background-color", switchColor)
+	document.body.style.cursor = 'pointer';
+})
+
+// Reverse Mouseover Changes when Mouse Leaves
+$navButton.mouseleave(function(){
+	$(this).css("background-color", "rgba(240,240,240,0)")
+	document.body.style.cursor = 'default';
+})
