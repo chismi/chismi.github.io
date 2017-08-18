989997a852f989060b1bb28701f71eae2d96d0ce
Mitch Chisholm <chisholm.mitchell@gmail.com>
2017-08-18 11:11:33 -0400

    updated package.json file

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp = require('gulp'),
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee'),
+	concat = require('gulp-concat');
+	browserify = require('gulp-browserify');
+	compass = require('gulp-compass');
+	connect = require('gulp-connect');
