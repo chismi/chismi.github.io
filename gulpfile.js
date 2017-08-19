ec5f0d361bef488a4ecfc11006f3eabeb3ec3e45
Mitch Chisholm <chisholm.mitchell@gmail.com>
2017-08-18 21:22:29 -0400

    added compass function

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,14 @@ var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
 	concat = require('gulp-concat'),
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify'),
+	compass = require('gulp-compass');
 
 
 var coffeeSources = ['components/coffee/*.coffee'];
 var jsSources = ['components/scripts/descriptions.js'];
+var sassSources = ['components/sass/style.scss'];
+
 
 gulp.task('coffee',function() {
 	gulp.src(coffeeSources)
@@ -22,3 +25,13 @@ gulp.task('js',function() {
 		.pipe(gulp.dest('builds/js'))
 });
 
+gulp.task('compass',function() {
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/images',
+			style: 'expanded'
+		}))
+		.on('error',gutil.log)
+		.pipe(gulp.dest('builds/css'))
+});
