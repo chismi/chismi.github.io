dc48ed60cc71b4d9e0d8b46b68089068cd614d1e
Mitch Chisholm <chisholm.mitchell@gmail.com>
2017-08-18 20:49:50 -0400

    added coffee funcitons to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,12 @@
 var gulp = require('gulp'),
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
-	browserify = require('gulp-browserify');
-	compass = require('gulp-compass');
-	connect = require('gulp-connect');
+
+var coffeeSources = ['components/coffee/*.coffee'];
+
+gulp.task('coffee',function() {
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error',gutil.log))
+		.pipe(gulp.dest('components/scripts'))
+});
